4ddd1c21674d7708d14a2e2c7a449969d178ee48
Dan cannon <capnhowdy21@hotmail.com>
2014-03-28 23:12:11 -0400

    - added test Servlet for Ajax Get - added responsetest.html

---
+++ src/java/test/AjaxTest.java
@@ -0,0 +1,71 @@
+/*
+ * 
+ */
+
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Dan Cannon
+ */
+@WebServlet(name = "AjaxTest", urlPatterns = {"/ajaxtest"})
+public class AjaxTest extends HttpServlet
+{
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        Enumeration names = request.getParameterNames();
+        while(names.hasMoreElements()){
+            String str = (String)names.nextElement();
+            writer.print(str + ": " + request.getParameter(str) + "<br />");
+            System.out.println("");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo()
+    {
+        return "Short description";
+    }
+
+}
